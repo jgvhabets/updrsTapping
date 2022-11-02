0e25e787d8bf7375d067954583b860653e358025
jgvhabets <jgvhabets@gmail.com>
2022-11-02 11:50:24 +0100

    ENH: added func to rename DUS data in same structure

---
+++ DUS_states_renaming.py
@@ -0,0 +1,40 @@
+"""
+Renaming function
+
+Run in python to standardise the medstim-state
+coding of the acc files.
+Perform before cutting so the cutted blocks
+have the correct M0S0 naming
+"""
+if __name__ == 'main':
+    # import necessary functions
+    import os
+
+    from retap_utils import utils_dataManagement
+
+    from pandas import read_csv, DataFrame
+
+    path = utils_dataManagement.find_stored_data_path('DUS')
+
+    
+    for f in os.listdir(path):
+
+        if f[-3:] != 'csv': continue
+
+        sub = f.split('_')[0]
+        med = f.split('_')[1]
+        stim = f.split('_')[3]
+
+        if med == 'OFF': med = 'M0'
+        elif med == 'ON': med = 'M1'
+
+        if 'nostim' in stim.lower(): stim = 'S0'
+        else: stim = 'S1'
+
+        f_new = f'{sub}_{med}{stim}.csv'
+
+        data = read_csv(os.path.join(path, f))
+
+        data.to_csv(os.path.join(path, 'RENAMED', f_new))
+
+        print(f'saved" {f_new}')
--- uncut_file_renaming.py
+++ uncut_file_renaming.py
@@ -6,49 +6,49 @@ coding of the acc files.
 Perform before cutting so the cutted blocks
 have the correct M0S0 naming
 """
+if __name__ == 'main':
+    # import necessary functions
+    import os
 
-# import necessary functions
-import os
+    from retap_utils import utils_dataManagement
 
-from retap_utils import utils_dataManagement
+    uncut_path = utils_dataManagement.find_stored_data_path('uncut')
 
-uncut_path = utils_dataManagement.find_stored_data_path()
-
-state_strings = {
-    'M0S0': [
-        'MedOffStimOff',
-        'MOffSOff',
-        'medOff_stimOff'
-    ],
-    'M0S1': [
-        'MedOffStimOn',
-        'MOffSOn',
-        'medOff_stimOn'
-    ],
-    'M1S0': [
-        'MedOnStimOff',
-        'MOnSOff',
-        'medOn_stimOff'
-    ],
-    'M1S1': [
-        'MedOnStimOn',
-        'MOnSOn',
-        'medOn_stimOn'
-    ]
-}
+    state_strings = {
+        'M0S0': [
+            'MedOffStimOff',
+            'MOffSOff',
+            'medOff_stimOff'
+        ],
+        'M0S1': [
+            'MedOffStimOn',
+            'MOffSOn',
+            'medOff_stimOn'
+        ],
+        'M1S0': [
+            'MedOnStimOff',
+            'MOnSOff',
+            'medOn_stimOff'
+        ],
+        'M1S1': [
+            'MedOnStimOn',
+            'MOnSOn',
+            'medOn_stimOn'
+        ]
+    }
 
 
-for f in os.listdir(uncut_path):
+    for f in os.listdir(uncut_path):
 
-    for state in state_strings:
+        for state in state_strings:
 
-        for string in state_strings[state]:
-            if string in f:
-                f_new = f.replace(string, state)
-                print('rename', f)
-                os.rename(
-                    os.path.join(uncut_path, f),
-                    os.path.join(uncut_path, f_new)
-                )
-                print('\tinto', f_new)
+            for string in state_strings[state]:
+                if string in f:
+                    f_new = f.replace(string, state)
+                    print('rename', f)
+                    os.rename(
+                        os.path.join(uncut_path, f),
+                        os.path.join(uncut_path, f_new)
+                    )
+                    print('\tinto', f_new)
 
