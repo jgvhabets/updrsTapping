df2b7077140e2f5d4a68d84ca38537f6c9506bd4
jgvhabets <jgvhabets@gmail.com>
2022-11-10 12:54:58 +0100

    ENH: fixed functionality for DUS data and Matlab-based CSVs

--- tap_extract_fts/main_featExtractionClass.py
+++ tap_extract_fts/main_featExtractionClass.py
@@ -10,10 +10,10 @@ import os
 from dataclasses import dataclass, field
 from typing import Any
 from itertools import product
-from pandas import read_csv, read_excel
-from numpy import logical_and
+from pandas import read_csv
+from numpy import logical_and, isnan, loadtxt
 
-from retap_utils import utils_dataManagement
+from retap_utils import utils_dataManagement as utils_dataMangm
 from tap_extract_fts import tapping_extract_features as ftExtr
 import tapping_run as tap_finder
 
@@ -34,13 +34,16 @@ class FeatureSet:
     sides: list = field(
         default_factory=lambda: ['L', 'R'])
     incl_meta_data: bool = True
+    skipped_no_meta: list = field(default_factory=list)
     verbose: bool = False
 
     def __post_init__(self,):
 
         for cen in self.centers_incl:
+
             if self.verbose: print(f'start with {cen}')
-            datapath = utils_dataManagement.find_stored_data_path(cen)
+            
+            datapath = utils_dataMangm.find_stored_data_path(cen)
 
             if self.subs_incl == 'ALL':
                 subs = list(set(
@@ -52,12 +55,15 @@ class FeatureSet:
             
             # import participant log data
             if self.incl_meta_data:
-                log = get_participantLog(cen)
+                log = utils_dataMangm.get_participantLog(cen)
                 meta = True
             else: meta = False
             
-            for sub in subs:
-                if self.verbose: print(f'\tSTART sub: {sub}')
+            for n_sub, sub in enumerate(subs):
+                print(
+                    f'\textracting sub {sub} ('
+                    f'{round((n_sub + 1) / len(subs) * 100)} % of {cen})'
+                )
 
                 if meta: sublog = log[[
                     str(s).upper() == sub.upper() for s in log["subID"]
@@ -76,20 +82,21 @@ class FeatureSet:
                     combo_files = list(set(
                         [f for f in subfiles if state in f]
                     ))
-                    # for BER data, select on side (DUS is unilat)
-                    if cen == 'BER':
-                        combo_files = list(set(
-                            [f for f in combo_files if f'_{side}_' in f]
-                        ))
+                    # select on side (DUS is renamed incl side)
+                    combo_files = list(set(
+                        [f for f in combo_files if f'_{side}_' in f]
+                    ))
 
                     # get META for correct med and stim state
-                    if meta: combolog = sublog[logical_and(
-                        sublog['medStatus'] == int(state[1]),
-                        sublog['stimStatus'] == int(state[3])
-                    )].reset_index(drop=True)
+                    if meta:
+                        combolog = sublog[logical_and(
+                            sublog['medStatus'] == int(state[1]),
+                            sublog['stimStatus'] == int(state[3])
+                        )].reset_index(drop=True)
 
-                    if cen == 'BER':  # get meta for correct side
-                        if meta: combolog = combolog[
+                    # get meta for correct side (DUS renamed)
+                    if meta:
+                        combolog = combolog[
                             [side[0].lower() in s for s in combolog['side']]
                         ].reset_index(drop=True)
                     
@@ -97,11 +104,11 @@ class FeatureSet:
                     if len(combo_files) == 0:
                         if self.verbose: print(f'no FILES found for {state, side}')
                         continue
-                    # elif len(combolog) == 0:
-                    #     if self.verbose: print(f'no SCORES found for {state, side}')
-                    #     continue
+                    elif len(combolog) == 0:
+                        if self.verbose: print(f'no SCORES found for {state, side}')
+                        continue
                         
-
+                    # LOOP AND INCLUDE ALL REPETITIONS PER COMBI
                     for n, f in enumerate(combo_files):
                         # find repetition of sub-state-side
                         if f.split('_')[-2][:5] == 'block':
@@ -115,23 +122,49 @@ class FeatureSet:
                                 tap_score = int(combolog[
                                     combolog['repetition'] == rep
                                 ]['updrsFt'])
-                                
+                            
+                            except ValueError:
+                                if isnan(
+                                    combolog[combolog['repetition'] == rep]['updrsFt']
+                                 ).item():
+                                    print(
+                                        '\tskip extraction - NaN-score '
+                                        f'{sub}_{state}_{side}_{rep}'
+                                    )
+                                    self.skipped_no_meta.append(
+                                        f'{sub}_{state}_{side}_{rep}'
+                                    )
+                                    continue
+
                             except KeyError:
-                                # print('meta not available in log for '
-                                #       f'{sub}_{state}_{side}_{rep}')
+                                print(
+                                        '\tskip extraction - missing-score '
+                                        f'{sub}_{state}_{side}_{rep}'
+                                    )
+                                self.skipped_no_meta.append(
+                                    f'{sub}_{state}_{side}_{rep}'
+                                )
                                 continue
                         
                             except TypeError:
                                 if combolog.shape[0] == 0:
+                                    print(
+                                        '\tskip extraction - missing-score '
+                                        f'{sub}_{state}_{side}_{rep}'
+                                    )
+                                    self.skipped_no_meta.append(
+                                        f'{sub}_{state}_{side}_{rep}'
+                                    )
                                     continue
                                 else:
                                     print(f'REP is {rep}, {sub, state, side}')
                                     print(combolog)
                                     raise TypeError('typeError not because of empty combolog DF')
-                            
+                        
+                        # if meta data is not wanted
                         else:
                             tap_score = None
-
+                        
                         setattr(
                             self,
                             f'{sub}_{state}_{side}_{rep}',
@@ -164,14 +197,20 @@ class singleTrace:
     to_extract_feats: bool = True
 
     def __post_init__(self,):
-        # store only np-array as acc-signal
-        dat = read_csv(self.filepath, index_col=False)
-        # delete index col without heading if present
-        if 'Unnamed: 0' in dat.keys():
-            del(dat['Unnamed: 0'])
-            dat.to_csv(self.filepath, index=False)
-        # set data to attribute
-        setattr(self, 'acc_sig', dat.values.T)
+        # load and store tri-axial ACC-signal
+        if self.center == 'BER':
+            dat = read_csv(self.filepath, index_col=False)
+            # delete index col without heading if present
+            if 'Unnamed: 0' in dat.keys():
+                del(dat['Unnamed: 0'])
+                dat.to_csv(self.filepath, index=False)
+            dat = dat.values.T  # only np-array as acc-signal
+
+        elif self.center == 'DUS':  # matlab-saved CSV-files
+            dat = loadtxt(self.filepath, delimiter='\t')
+
+        # set data to attribute (3 rows, n-samples columns)
+        setattr(self, 'acc_sig', dat)
 
         # extract sample freq if given
         if 'hz' in self.filepath.lower():
@@ -180,7 +219,7 @@ class singleTrace:
             self.fs = int(fs)
         else:
             self.fs = 250  # defaults to 250 if not defined in filename
-        
+
         if self.to_extract_feats:
 
             tap_idx, impact_idx, _ = tap_finder.run_updrs_tap_finder(
@@ -197,32 +236,6 @@ class singleTrace:
                 tapDict=tap_idx,
                 updrsSubScore=self.tap_score,
             )
-            print(self.impact_idx)
-
-
 
         
 
-def get_participantLog(center = ['DUS', 'BER']):
-    """
-    Get Excel file with participant
-    meta data "ReTap_participantLog.xlsx"
-
-    Input:
-        - center: optional, DUS or BER, defaults
-            to both. Defines which excel-sheets
-            are imported
-
-    Returns:
-        - log: dict containing the BER and DUS
-            sheet, each in one DataFRame in dict
-    """
-    p = utils_dataManagement.find_stored_data_path('retapdata')
-    xl_fname = 'ReTap_participantLog.xlsx'
-
-    log = read_excel(
-        os.path.join(p, xl_fname),
-        sheet_name=center
-    )
-
-    return log
